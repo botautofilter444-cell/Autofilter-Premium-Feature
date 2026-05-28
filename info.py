6048295cc53a41e417fa2f02d07e6cfdefa9803d
botautofilter444-cell <botautofilter444@gmail.com>
2026-05-28 09:23:48 +0530

    Update info.py

--- info.py
+++ info.py
@@ -121,7 +121,7 @@ THREE_VERIFY_GAP = int(environ.get('THREE_VERIFY_GAP', "54000"))
 # Channel & Group Links Configuration
 # ============================
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/Epic_Supports') # Group link for the bot
-OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/KingKishan') # Owner link for the bot
+OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/Rider_Epic') # Owner link for the bot
 UPDATE_CHNL_LNK = environ.get('UPDATE_CHNL_LNK', 'https://t.me/EpicCastle') # Update channel link for the bot
 
 # ============================
